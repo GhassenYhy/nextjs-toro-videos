e91510ff47da18d8cc90fe1801ab5017bb2cfc1e
Ghassen <ghassen@briqbookings.com>
2022-07-29 22:42:21 +0200

    Home page created

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["freeiconshop.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
