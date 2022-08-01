0057cf8f5034dd99120f1d07903a5d4697a925ab
Ghassen <ghassen@briqbookings.com>
2022-08-01 13:33:45 +0200

    Implemented Like feature

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -29,6 +29,12 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
     }
   };
 
+  useEffect(() => {
+    if (videoRef?.current) {
+      videoRef.current.muted = isVideoMuted;
+    }
+  }, [isVideoMuted]);
+
   return (
     <div className="flex flex-col border-b-2 border-gray-200 pb-6">
       <div>
@@ -73,7 +79,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
               src={post.video.asset.url}
               ref={videoRef}
               loop
-              className="lg:w-[600px] h-[300px] md:h-[500px] rounded-2xl cursor-pointer bg-gray-100"
+              className="lg:w-[600px] h-[300px] md:h-[600px] rounded-2xl cursor-pointer bg-gray-100"
             />
           </Link>
           {isHover && (
